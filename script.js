4ed3649396a473e27ae1cefb2b29bbf9cb39295d
Alfred Soll <mrmuffin085@gmail.com>
2025-10-30 09:12:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const clientId = "1433054522170802342"; // Discord app
-const redirectUri = "https://alfredsoll.github.io/pet/dashboard.html";
 
-document.getElementById("loginBtn").addEventListener("click", () => {
-    const scope = "identify guilds";
-    const discordAuthUrl = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=token&scope=${encodeURIComponent(scope)}`;
-    window.location.href = discordAuthUrl;
-});
